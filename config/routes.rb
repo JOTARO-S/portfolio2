5c839f67851d991db562b0d8ad13b39d9c3303e2
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2023-01-14 21:25:36 +0000

    devise インストール＆設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root to: "homes#top"
+  end
+  scope module: :public do
+    root to: "homes#top"
+  end
+# 顧客用
+devise_for :customer, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions',
+  passwords: 'public/passwords'
+}
+
+# 管理者用
+devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
